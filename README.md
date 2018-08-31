84a1a9e3b67a1e50fdc0f9c3b385ac3de18d4411
Srinivas Kalyani <Srinivaskalyani12@gmail.com>
2018-08-31 14:08:13 +0530

    fix: boilerplate

--- README.md
+++ README.md
@@ -8,14 +8,14 @@ There are two methods for getting started with this repo.
 Checkout this repo, install dependencies, then start the gulp process with the following:
 
 ```
-> git clone https://github.com/shailsethiya/ReactSimpleApp.git
-> cd ReactSimpleApp
+> git clone https://github.com/shailsethiya/ReduxApplication.git
+> cd ReduxApplication
 > npm install
 > npm start
 ```
 
 #### Not Familiar with Git?
-Click [here](https://github.com/shailsethiya/ReactSimpleApp.git/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
+Click [here](https://github.com/shailsethiya/ReduxApplication/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
 
 ```
 > npm install
--- src/index.js
+++ src/index.js
@@ -1,44 +1,15 @@
-import _ from 'lodash';
-import React,{Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
-import YTSearch from 'youtube-api-search';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-const API_KEY = 'AIzaSyAybNGSHY6K03dtuGcxG6Ng4QYGKa3kHkM';
+import App from './components/app';
+import reducers from './reducers';
 
-class App extends Component{
-	constructor(props){
-		super(props);
-		this.state = { 
-			videos : [],
-            selectedVideo: null
-		};
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-		this.videoSearch('surfboards');
-	    }
-   
-
-         videoSearch(term){
-            YTSearch({key: API_KEY, term: term}, (videos) => {
-			this.setState({ 
-				videos: videos,
-                selectedVideo: videos[0]
-			 })
-		});
-        }
-
-  render() {
-  	const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
-	 return(
-	 	<div> 
-	 	   <SearchBar onSearchTermChange={videoSearch}/>
-	 	   <VideoDetail video={this.state.selectedVideo} />
-	 	   <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos ={this.state.videos}/>
-	 	</div>
-		);
-	}
-}
-
-ReactDOM.render(<App /> , document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
